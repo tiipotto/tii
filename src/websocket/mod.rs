779b002faa342794bff2983d62f8f0bf17ac6b88
Kevin Nakamura <grinkers@grinkers.net>
2024-10-19 00:47:33 +0900

    Basic README describing the new fork. Add current `authors` Cargo.toml and LICENSE.

--- src/websocket/mod.rs
+++ src/websocket/mod.rs
@@ -1,8 +1,6 @@
 //! Humpty WebSocket is a crate which extends Humpty Core with WebSocket support by hooking into the latter's `WebsocketHandler` trait. It handles the WebSocket handshake and framing protocol and provides a simple and flexible API for sending and receiving messages. Using Humpty's generic `Stream` type, it supports drop-in TLS. It also has no dependencies in accordance with Humpty's goals of being dependency-free.
 //!
 //! It provides both synchronous and asynchronous WebSocket functionality.
-//!
-//! Learn more about Humpty WebSocket [here](https://humpty.whenderson.dev/websocket/index.html).
 
 #![warn(missing_docs)]
 
