80347c6d99f5b71931455a8b74783ead92f44197
Kevin Nakamura <grinkers@grinkers.net>
2024-10-26 22:51:19 +0900

    shutdown example

--- examples/shutdown.rs
+++ examples/shutdown.rs
@@ -1,10 +1,70 @@
-fn main() {
-  // empty on purpose, the CI tests that this file is here, showdown is no longer something that the library cares about.
-  // the previous impl not great anyways (setting atomic boolean and connecting to localhost is not good, this can fail if there is port exhaustion)
-  // To implement this properly one has to call shutdown syscall on the rawfd of the TcpListen on unix.
-  // On windows its not possible with the sockets provided by the STL because they do not use overlapped (interruptible) WSOCK2 io.
-  // Its my honest opinion that outsourcing this to the user is best. They can use a third party socket library that implements this properly
-  // On windows and I can just call shutdown on the fd in my unix code.
+use humpty::http::request_context::RequestContext;
+use humpty::http::{Response, StatusCode};
+use humpty::humpty_builder::HumptyBuilder;
+
+use std::error::Error;
+use std::net::{self, IpAddr, SocketAddr, TcpListener, TcpStream, ToSocketAddrs};
+use std::sync::mpsc;
+use std::thread::{sleep, spawn};
+use std::time::Duration;
+use std::{io, thread};
+
+fn hello(_: &RequestContext) -> io::Result<Response> {
+  Ok(Response::new(StatusCode::OK, "<html><body><h1>Hello</h1></body></html>"))
+}
+
+fn unspecified_socket_to_loopback<S>(socket: S) -> SocketAddr
+where
+  S: ToSocketAddrs,
+{
+  let mut socket = socket.to_socket_addrs().unwrap().next().unwrap(); // This can't fail, because the server was able to start.
+  if socket.ip().is_unspecified() {
+    match socket.ip() {
+      IpAddr::V4(_) => socket.set_ip(IpAddr::V4(net::Ipv4Addr::new(127, 0, 0, 1))),
+      IpAddr::V6(_) => socket.set_ip(IpAddr::V6(net::Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0x1))),
+    };
+  }
+  socket
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+  let (shutdown_app, app_rx) = mpsc::sync_channel(1);
+
+  let app = HumptyBuilder::default()
+    .router(|router| router.with_route("/*", hello))
+    .with_connection_timeout(Some(Duration::from_secs(5)))
+    .build_arc();
+
+  let listen = TcpListener::bind("0.0.0.0:8080")?;
+  let addr = listen.local_addr()?;
+  println!("successfully listening on {addr}");
+
+  // Send shutdown signal after 5 seconds, well after threads have started working
+  let t = spawn(move || {
+    sleep(Duration::from_secs(5));
+    shutdown_app.send(()).unwrap();
+    TcpStream::connect(unspecified_socket_to_loopback(addr)).unwrap(); // wake up the TcpListener loop
+  });
+
+  for stream in listen.incoming() {
+    if app_rx.try_recv().is_ok() {
+      println!("shutdown receieved. breaking out of loop");
+      break;
+    }
+    let app = app.clone();
+    thread::spawn(move || {
+      app.handle_connection(stream?).expect("ERORR");
+      Ok::<(), io::Error>(())
+    });
+  }
+
+  // The TcpListener can be dropped. Within 5 seconds, it should always be free to be reused.
+  drop(listen);
+  let _listen = TcpListener::bind("0.0.0.0:8080")?;
+
+  // Wait for thread to fully finish. Unneeded but placed here for full memory tests.
+  t.join().unwrap();
+  Ok(())
 }
 
 #[test]
